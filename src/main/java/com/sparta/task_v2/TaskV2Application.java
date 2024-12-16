707e7a2d782b499118e6891abcc1a4f596df7a3f
DESKTOP-APA79D3\kimmi <kimminsik0726@gmail.com>
2024-12-16 11:24:05 +0900

    init: 프로젝트 설정

---
+++ src/main/java/com/sparta/task_v2/TaskV2Application.java
@@ -0,0 +1,13 @@
+package com.sparta.task_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskV2Application.class, args);
+    }
+
+}
